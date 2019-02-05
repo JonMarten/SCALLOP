f7b858b0b8ded011b4a937bdbc3a97167998d41f
Jonathan Marten <jonmarten@gmail.com>
2019-02-05 17:09:00 +0000

    Iterate phenoscanner over all SNPS.

--- scripts/inf1_annotate_v01.r
+++ scripts/inf1_annotate_v01.r
@@ -1,8 +1,14 @@
-# Mk. I annotation script
+# Mk. I annotation script. Lookup clumped hits from METAL in phenoscanner.
+
+# Get phenoscanner
+#install.packages("devtools")
+#library(devtools)
+#install_github("phenoscanner/phenoscanner")
+#library(phenoscanner)
 
 # Set WD and set up libraries
 setwd("C:/Users/Jonathan/Documents/CEU/R_projects/SCALLOP/")
-lapply(c("data.table", "dplyr", "stringr"), require, character.only = T)
+lapply(c("data.table", "dplyr", "stringr", "phenoscanner"), require, character.only = T)
 
 # Read in INF1 results
 clumpedPath <- "Z:/Factors/High_dimensional_genetics/Olink/INF1/INF1.clumped.tbl"
@@ -10,7 +16,31 @@ inf1 <- fread(clumpedPath, data.table=F)
 
 # Reform columns to remove hyphens and separate data
 prot <- str_split_fixed(inf1$Chromosome, ":", 2)
-inf1$Chromosome <- as.numeric(prot[,2])
-inf1$Protein <- prot[,1]
 markerName <- str_split_fixed(inf1$MarkerName,"_",2)
-inf1$psName <- markerName[,1]
+inf1 <- inf1 %>%
+  mutate(Chromosome = as.numeric(prot[,2]),
+         Protein = prot[,1],
+         psName = markerName[,1]) %>%
+  select(Protein, psName, Chromosome:N)
+
+#Lookup in phenoscanner
+infAnnotated <- data.frame()
+i = 1
+for(i in 1:nrow(inf1)){
+  infRow <- inf1[i,]
+  gwas <- phenoscanner(snpquery = inf1$psName[i], catalogue = "GWAS")
+  eQTL <- phenoscanner(snpquery = inf1$psName[i], catalogue = "eQTL")
+  pQTL <- phenoscanner(snpquery = inf1$psName[i], catalogue = "pQTL")
+  
+  infRow <- infRow %>%
+    mutate(rsid = gwas$snps$rsid,
+           consequence = gwas$snps$consequence,
+           hgnc = gwas$snps$hgnc,
+           gwas = paste(unlist(unique(gwas$results$trait)), collapse = "; "),
+           eQTL = paste(unlist(unique(eQTL$results$trait)), collapse = "; "),
+           pQTL = paste(unlist(unique(pQTL$results$trait)), collapse = "; "))
+  infAnnotated <- bind_rows(infAnnotated, infRow)
+  rm(gwas, eQTL, pQTL,infRow)
+}
+
+
