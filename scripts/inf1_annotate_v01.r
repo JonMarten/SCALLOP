ef3444954263ad47a5eb61748d758990eb8d35de
Jonathan Marten <jonmarten@gmail.com>
2019-02-27 17:29:05 +0000

    Update code for heatmap.

--- scripts/inf1_annotate_v01.r
+++ scripts/inf1_annotate_v01.r
@@ -1,14 +1,14 @@
 # Mk. I annotation script. Lookup clumped hits from METAL in phenoscanner.
-
-# Get phenoscanner
+# Install phenoscanner ----------------------------------------------------
 #install.packages("devtools")
 #library(devtools)
 #install_github("phenoscanner/phenoscanner")
 #library(phenoscanner)
 
+# Set up and read in data -------------------------------------------------
 # Setup
   setwd("C:/Users/Jonathan/Documents/CEU/R_projects/SCALLOP/")
-  lapply(c("data.table", "dplyr", "stringr", "phenoscanner", "pheatmap"), require, character.only = T)
+  lapply(c("data.table", "dplyr", "stringr", "phenoscanner", "pheatmap", "reshape2"), require, character.only = T)
 
 # Read in INF1 results
   clumpedPath <- "Z:/Factors/High_dimensional_genetics/Olink/INF1/INF1.clumped.tbl"
@@ -22,9 +22,13 @@
            Protein = prot[,1],
            psName = markerName[,1]) %>%
     select(Protein, psName, Chromosome:N)
+  rm(prot, markerName, clumpedPath)
 
+# Naive phenoscanner run, one per SNP per protein -------------------------
 # Submit SNPs to phenoscanner, 100 at a time
-  rm(psResults)
+  if(exists("psResults")){ 
+    rm(psResults)
+  }
   for(i in 1:ceiling(nrow(inf1)/100)){
     startrow <- (i-1)*100 + 1
     endrow <- min(nrow(inf1), (i-1)*100 + 100)
@@ -116,13 +120,15 @@ psResults2 <- psResults %>%
          unit.ps)
          
 
-fwrite(psResults2, file = "SCALLOP_INF1_phenoscanner_gwas_allResults.csv", na = "NA")
- 
+fwrite(psResults2, file = "Y:/Projects/SCALLOP/INF1/SCALLOP_INF1_phenoscanner_gwas_allResults.csv", na = "NA")
+
+
+# Simplified run, running pleiotropic SNPs once only ----------------------
 ### Simplified query combining SNPs that are pQTLs for multiple proteins
 
 infSmall <- inf1 %>% select(Protein,psName) %>% 
   group_by(psName) %>% 
-  summarise(numAssocs = n(), prots = paste(Protein, collapse = ";")) %>%
+  summarise(numAssocs = n(), prots = paste(Protein, collapse = "; ")) %>%
   data.frame()
 
 rm(psResultsSmall)
@@ -160,35 +166,119 @@ for(i in 1:ceiling(nrow(infSmall)/100)){
   rm(ps, startrow, endrow)
 }  
 
-# Merge phenoscanner output with infSmall
-psResultsSmall <- right_join(psResultsSmall$snps, psResultsSmall$results)
-names(psResultsSmall) <- paste0(names(psResultsSmall),".ps")
-psResultsSmall <- rename(psResultsSmall, psName = snp.ps)
-psResultsSmall <- full_join(infSmall, psResultsSmall)
-psResultsSmall <- distinct(psResultsSmall) # remove duplicated rows, not sure why these appear and too lazy to figure it out right now.
+# Cpnvert phenoscanner output to data frame instead of list
+  psResultsSmall <- right_join(psResultsSmall$snps, psResultsSmall$results)
+  
+# Where a SNP is assocaited with a trait more than once, pick EA first, then largest study, or if no n is available, pick smallest P-value
+  psResultsSmall <- psResultsSmall %>%
+    mutate(snpTrait = paste0(snp, trait))
+  
+  dupes <- duplicated(psResultsSmall$snpTrait)
+  dupePhenos <- unique(psResultsSmall$snpTrait[dupes])
+  #dupeRows <- which(psResultsSmall$snpTrait %in% dupePhenos)
+  
+  dupesFiltered <- data.frame()
+  for(i in 1:length(dupePhenos)){
+    subset <- psResultsSmall %>% filter(snpTrait == dupePhenos[i])
+    eaRows <- which(subset$ancestry == "European")
+    if(length(eaRows) == 1){
+      keeprow <- subset[eaRows,]
+    } else {
+      if (length(eaRows) > 1){
+        subset <- subset[eaRows,]
+        keeprow <- subset[which.max(na.exclude(subset$n)),]
+        if(length(na.exclude(subset$n))>0) {
+          keeprow <- subset[which.max(na.exclude(subset$n)),]
+        } else {
+          keeprow <- subset[which.min(na.exclude(subset$p)),]
+        }
+      } else {
+        if(length(na.exclude(subset$n))>0) {
+          keeprow <- subset[which.max(na.exclude(subset$n)),]
+        } else {
+          keeprow <- subset[which.min(na.exclude(subset$p)),]
+        }
+      }
+    }
+    dupesFiltered <- rbind(dupesFiltered, keeprow)
+    rm(keeprow, subset, eaRows)
+  }
+  
+psResultsSmall2 <- psResultsSmall #backup   
+psResultsSmall <- psResultsSmall %>%
+  filter(!snpTrait %in% dupePhenos)
+psResultsSmall <- rbind(psResultsSmall, dupesFiltered)
+    
+# Merge phenoscanner output with infSmall  
+  names(psResultsSmall) <- paste0(names(psResultsSmall),".ps")
+  psResultsSmall <- rename(psResultsSmall, psName = snp.ps)
+  psResultsSmall <- full_join(infSmall, psResultsSmall)
+  psResultsSmall <- distinct(psResultsSmall) # remove duplicated rows, not sure why these appear and too lazy to figure it out right now.
 
 # Look up proxies for SNPs with no results found  
 #  missRow <- which(inf1$psName %in% psResults2$psName)
 #  inf1Proxy <- inf1[missRow,]
 #  psProxy <- phenoscanner(inf1Proxy$psName, proxies = "EUR", r2 = 0.8)
 
-psResultsSmall$rsid.ps[which(is.na(psResultsSmall$rsid.ps))] <- psResultsSmall$psName[which(is.na(psResultsSmall$rsid.ps))]
-psResultsSmall$snpProt <- paste0(psResultsSmall$rsid.ps," (",psResultsSmall$prots,")")
+# Generate "RSID (protein)" identiers for each association
+  noRS <- which(is.na(psResultsSmall$rsid.ps))
+  psResultsSmall$rsid.ps[noRS] <- psResultsSmall$psName[noRS]
+  psResultsSmall <- psResultsSmall %>%
+    mutate(snpProt = paste0(rsid.ps," (gene: ",hgnc.ps,", Olink: ", prots,")"))
 
+# List all SNPs and their associated traits  
+  snpProtSummary <- psResultsSmall %>% 
+    group_by(snpProt) %>%
+    summarise(n(),
+              traits = paste(unique(trait.ps), collapse = "; ")) %>% 
+    data.frame
+# List all traits and their proteins 
+  traitSummary <- psResultsSmall %>% 
+    group_by(trait.ps) %>%
+    summarise(n(),
+              Proteins = paste(unique(prots), collapse = "; ")) %>% 
+    data.frame()
+
+# Convert to a contingency table for easy pheatmapping
 psMeltSmall <- psResultsSmall %>%
   select(snpProt, trait.ps, p.ps) %>%
   mutate(logP = -log10(p.ps)) %>%
-  select(-p.ps) %>%
-  filter(trait.ps %in% names(which(table(psMelt$trait.ps) > 10)))
+  select(-p.ps)
+
+psMeltSmall <- psMeltSmall %>%
+  filter(!is.na(logP)) %>%  
+  distinct()
+# Filter to only traits associated with >2 SNPs
+psMeltSmallFilt <- psMeltSmall %>%
+  filter(trait.ps %in% names(which(table(psMeltSmall$trait.ps) > 2))) %>%
+  filter(!is.na(logP)) %>%  
+  distinct()
+
   
-psCastSmall <- acast(psMelt, snpProt ~ trait.ps)  
+psCastSmallFilt <- acast(psMeltSmallFilt, snpProt ~ trait.ps, fun.aggregate = function(x){x[1]}) # why max doesn't work here I have no idea, but it just returns Inf every time. 
+psCastSmall <- acast(psMeltSmall, snpProt ~ trait.ps, fun.aggregate = function(x){x[1]})
 
 
-pheatmap(psCastSmall, color = colorRampPalette(c("white","firebrick2"))(200), cex = 0.2)
+
+psCastSmallFilt[which(is.na(psCastSmallFilt), arr.ind = T)] <- 0
+psCastSmallFilt[which(is.infinite(psCastSmallFilt), arr.ind = T)] <- 300
+psCastSmall[which(is.na(psCastSmall), arr.ind = T)] <- 0
+psCastSmall[which(is.infinite(psCastSmall), arr.ind = T)] <- 300
+plotNums <- signif(psCastSmallFilt, 2)
+
+pdf(file = "Y:/Projects/SCALLOP/INF1/SCALLOP_INF1_phenoscanner_gwas_small_heatmap_2plus.pdf", height = 20, width = 20)
+  pheatmap(t(psCastSmallFilt), display_numbers = t(plotNums), color = colorRampPalette(c("white","firebrick2"))(200), cex = 1)
+dev.off()
+
+pdf(file = "Y:/Projects/SCALLOP/INF1/SCALLOP_INF1_phenoscanner_gwas_small_heatmap.pdf", height = 30, width = 20)
+pheatmap(t(psCastSmall), display_numbers = t(signif(psCastSmall, 2)), color = colorRampPalette(c("white","firebrick2"))(200), cex = 1)
+dev.off()
+
+write.csv(psResultsSmall, row.names=F, file = "Z:/Factors/High_dimensional_genetics/Olink/INF1/phenoscanner/SCALLOP_INF1_phenoscanner_gwas_annotations.csv")
 
 psResults %>% 
   group_by(Protein) %>%
-  summarise(n()) %>%
+  summarise(n()) %>% 
   data.frame()
 
 psResults2 <- psResults %>%
@@ -222,12 +312,10 @@ psResults2 <- psResults %>%
          unit.ps)
 
 
-fwrite(psResults2, file = "SCALLOP_INF1_phenoscanner_gwas_allResults.csv", na = "NA")
+fwrite(psResults2, file = "Y:/Projects/SCALLOP/INF1/SCALLOP_INF1_phenoscanner_gwas_allResults_small.csv", na = "NA")
 
+# Old Code ----------------------------------------------------------------
 
-
- 
-### Old code
     
 #Lookup in phenoscanner, 1 at a time
   # Initialise empty data frames
